f8dcad21aed7b5e5654a567d92ff8ed92c207ead
Sujith <sujithmsjs@gmail.com>
2026-03-29 21:08:04 +0530

    Global UI Added

--- src/app/(root)/qanotes/topics/components/TopicDeleteButton.jsx
+++ src/app/(root)/qanotes/topics/components/TopicDeleteButton.jsx
@@ -2,16 +2,21 @@
 
 import { useTransition } from 'react'
 import { deleteTopicAction } from '../actions'
+import { useModal } from '@/context/ModalContext';
 
 export default function TopicDeleteButton({ id }) {
   const [isPending, startTransition] = useTransition()
+  const { showModal } = useModal();
 
   const handleDelete = () => {
-    if (!confirm('Are you sure you want to delete this Topic?')) return
-    startTransition(async () => {
-      await deleteTopicAction(id)
-    })
-  }
+    showModal({
+      title: "Delete Note",
+      message: "Are you sure you want to delete this note?",
+      onConfirm: async () => startTransition(async () => {
+        await deleteTopicAction(id)
+      }),
+    });
+  };
 
   return (
     <button onClick={handleDelete} disabled={isPending} className="btn btn-xs btn-error">
